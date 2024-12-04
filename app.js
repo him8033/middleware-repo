7fbb96bdcc00fd4501a53a0809879476514169b6
him8033 <himanshu8033@gmail.com>
2024-12-04 14:11:27 +0530

    if you want to send a middleware in multiple api you normally create a function where define your middleware and simply pass in your desired api and if you want to send your generated error in any where simply using this 'throw new Error('')' in this function

--- app.js
+++ app.js
@@ -21,9 +21,35 @@ const port = 3000;
 //     next();
 // })
 
-app.use("/random",(req,res,next) => {                             //this type of middleware where path is define that means any of calling where path
-    console.log("I am only for random");                          //path is starting define path of middleware matches then this middleware perform 
-    next();                                                       //their tasks and if 'next()' is calling then forward to next middleware or their main request
+// app.use("/random",(req,res,next) => {                             //this type of middleware where path is define that means any of calling where path
+//     console.log("I am only for random");                          //path is starting define path of middleware matches then this middleware perform 
+//     next();                                                       //their tasks and if 'next()' is calling then forward to next middleware or their main request
+// })
+
+// app.use("/api",(req,res,next) => {                                  // this middleware is performing as access authenticator that means if user direct
+//     let {token} = req.query;                                        // calling this api request then user found a page there send "Access Denied!" page
+//     if(token==="giveaccess"){                                       // for getting the data user pass the query string where token value is "giveaccess"
+//         next();                                                     // if users token value is "giveaccess" then this middleware calling next function
+//     }else{                                                          // who's written in if condition and found their data are written in main api if 
+//         res.send("Access Denied!");                                 // token value is wrong or empty user found "Access Denied!" page
+//     }
+// })
+
+const checkToken = (req,res,next) => {                                  // if you not want to use that middleware in many of api,s then write these
+    let {token} =req.query;                                             //  type of middleware in a function and pass these function in those
+    if(token === "giveaccess"){                                         // particular api where you want to use these middleware
+        next();
+    }else{
+        throw new Error ("Access Denied!");
+    }
+}
+
+// app.get("/wrong",(req,res) => {                             //this is basic error whos generate default error whos written in express default error
+//     abcd = abcd;                                            // handler who generate a 500 error
+// })
+
+app.get("/api",checkToken,(req,res) => {                                //write the middleware function after defining path
+    res.send("data");
 })
 
 app.get("/",(req,res) => {
@@ -34,12 +60,14 @@ app.get("/random",(req,res) => {
     res.send("This is random");
 })
 
-app.use((req,res,next) => {
-    req.time = Date.now();
-    console.log(req.method,req.hostname,req.path,req.time);                     // these some Api 'req' references
-    // console.log(req);
-    next();
-})
+// // Logger
+
+// app.use((req,res,next) => {
+//     req.time = Date.now();
+//     console.log(req.method,req.hostname,req.path,req.time);                     // these some Api 'req' references
+//     // console.log(req);
+//     next();
+// })
 
 app.use((req,res) => {                                              // this type of middleware we are written in the last of all middleware 
     res.status(404).send("Page not found!");                        //if above middleware is not working perform then this execute and perform 
