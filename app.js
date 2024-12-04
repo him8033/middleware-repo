e702f6af905c1c245bac5c2f116bad1bb087812f
him8033 <himanshu8033@gmail.com>
2024-12-04 13:35:37 +0530

    some defined path middlewares and if any of path is not found then called error middleware for saying this page is not exist

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ const port = 3000;
 //     next();
 // })
 
-app.use((req,res,next) => {
-    console.log(req.method,req.hostname,req.path);
-    next();
+app.use("/random",(req,res,next) => {                             //this type of middleware where path is define that means any of calling where path
+    console.log("I am only for random");                          //path is starting define path of middleware matches then this middleware perform 
+    next();                                                       //their tasks and if 'next()' is calling then forward to next middleware or their main request
 })
 
 app.get("/",(req,res) => {
@@ -31,8 +31,19 @@ app.get("/",(req,res) => {
 })
 
 app.get("/random",(req,res) => {
-    res.send("This is random")
+    res.send("This is random");
 })
+
+app.use((req,res,next) => {
+    req.time = Date.now();
+    console.log(req.method,req.hostname,req.path,req.time);                     // these some Api 'req' references
+    // console.log(req);
+    next();
+})
+
+app.use((req,res) => {                                              // this type of middleware we are written in the last of all middleware 
+    res.status(404).send("Page not found!");                        //if above middleware is not working perform then this execute and perform 
+})                                                                  //those page you are searched is not found
 
 app.listen(port,() => {
     console.log("Server is listining on port:",port);
